8eac63209dae8ed85f68c1ce012c6698fd46bc79
CastroIROCZ <124112134+CastroIROCZ@users.noreply.github.com>
2023-08-06 23:29:44 -0700

    Update script.js
    Completed 3rd - 5th grade questions and answers

--- script.js
+++ script.js
@@ -130,19 +130,266 @@ const questions = {
     ]
   },
   "4th": {
-    Math: [ /* questions you provided go here */ ],
-    Science: [ /* and so on... */ ],
-    History: [ ],
-    English: [ ]
+    "Math": [
+      {
+        "question": "What is the sum of 5 + 7?",
+        "options": ["8", "10", "12", "14"],
+        "correctAnswer": "12"
+      },
+      {
+        "question": "Which number is the largest?",
+        "options": ["25", "35", "15", "45"],
+        "correctAnswer": "45"
+      },
+      {
+        "question": "What is 3 multiplied by 9?",
+        "options": ["18", "27", "21", "15"],
+        "correctAnswer": "27"
+      },
+      {
+        "question": "What is the place value of the digit '6' in the number 864?",
+        "options": ["Tens", "Hundreds", "Ones", "Thousands"],
+        "correctAnswer": "Hundreds"
+      },
+      {
+        "question": "What is the missing number in the pattern: 5, 10, __, 20, 25?",
+        "options": ["12", "15", "18", "22"],
+        "correctAnswer": "15"
+      },
+      {
+        "question": "What is the result of 64 divided by 8?",
+        "options": ["8", "6", "7", "9"],
+        "correctAnswer": "8"
+      }
+    ],
+    "Science": [
+      {
+        "question": "What state of matter is water in when it is frozen?",
+        "options": ["Solid", "Liquid", "Gas", "Plasma"],
+        "correctAnswer": "Solid"
+      },
+      {
+        "question": "What is the name of the closest star to Earth?",
+        "options": ["Moon", "Sun", "Venus", "Mars"],
+        "correctAnswer": "Sun"
+      },
+      {
+        "question": "Which animal is cold-blooded?",
+        "options": ["Elephant", "Penguin", "Lion", "Frog"],
+        "correctAnswer": "Frog"
+      },
+      {
+        "question": "What is the process by which plants make their own food using sunlight?",
+        "options": ["Photosynthesis", "Respiration", "Decomposition", "Transpiration"],
+        "correctAnswer": "Photosynthesis"
+      },
+      {
+        "question": "Which planet is known as the 'Red Planet'?",
+        "options": ["Earth", "Venus", "Mars", "Jupiter"],
+        "correctAnswer": "Mars"
+      },
+      {
+        "question": "What do you call the change of a caterpillar into a butterfly?",
+        "options": ["Hibernation", "Evolution", "Metamorphosis", "Pollination"],
+        "correctAnswer": "Metamorphosis"
+      }
+    ],
+    "History": [
+      {
+        "question": "Who was the first President of the United States?",
+        "options": ["Thomas Jefferson", "Abraham Lincoln", "George Washington", "John F. Kennedy"],
+        "correctAnswer": "George Washington"
+      },
+      {
+        "question": "What event led to the founding of the United States?",
+        "options": ["Boston Tea Party", "Declaration of Independence", "Invention of the Internet", "Construction of the Great Wall of China"],
+        "correctAnswer": "Declaration of Independence"
+      },
+      {
+        "question": "Which famous American explorer is known for his journey to the Pacific Ocean?",
+        "options": ["Christopher Columbus", "Lewis and Clark", "Marco Polo", "Ferdinand Magellan"],
+        "correctAnswer": "Lewis and Clark"
+      },
+      {
+        "question": "What did the Pilgrims celebrate with the Native Americans in 1621?",
+        "options": ["Halloween", "Thanksgiving", "Independence Day", "Christmas"],
+        "correctAnswer": "Thanksgiving"
+      },
+      {
+        "question": "Who wrote the Declaration of Independence?",
+        "options": ["George Washington", "Abraham Lincoln", "Thomas Jefferson", "Benjamin Franklin"],
+        "correctAnswer": "Thomas Jefferson"
+      },
+      {
+        "question": "Which famous civil rights leader gave the 'I Have a Dream' speech?",
+        "options": ["Martin Luther King Jr.", "Rosa Parks", "Malcolm X", "Harriet Tubman"],
+        "correctAnswer": "Martin Luther King Jr."
+      }
+    ],
+    "English": [
+      {
+        "question": "Which word is the antonym of 'happy'?",
+        "options": ["Sad", "Brave", "Funny", "Tall"],
+        "correctAnswer": "Sad"
+      },
+      {
+        "question": "Choose the correct spelling: 'frind', 'freind', 'friend', or 'frend'?",
+        "options": ["frind", "freind", "friend", "frend"],
+        "correctAnswer": "friend"
+      },
+      {
+        "question": "What is the meaning of the word 'beneath'?",
+        "options": ["above", "under", "behind", "around"],
+        "correctAnswer": "under"
+      },
+      {
+        "question": "Identify the correct sentence: 'She plays the piano.' or 'She play the piano.'?",
+        "options": ["She plays the piano.", "She play the piano."],
+        "correctAnswer": "She plays the piano."
+      },
+      {
+        "question": "What is the plural form of the word 'dog'?",
+        "options": ["dogs", "doggies", "dogies", "doggy"],
+        "correctAnswer": "dogs"
+      },
+      {
+        "question": "Which word means the opposite of 'night'?",
+        "options": ["light", "dark", "moon", "sleep"],
+        "correctAnswer": "light"
+      }
+    ]
   },
   "5th": {
-    Math: [ /* questions go here */ ],
-    Science: [ /* questions go here */ ],
-    History: [ ],
-    English: [ ]
+    "Math": [
+      {
+        "question": "What is 25 multiplied by 4?",
+        "options": ["50", "75", "100", "125"],
+        "correctAnswer": "100"
+      },
+      {
+        "question": "What is the place value of the digit '7' in the number 273?",
+        "options": ["Tens", "Hundreds", "Ones", "Thousands"],
+        "correctAnswer": "Tens"
+      },
+      {
+        "question": "Simplify the fraction: 12/18.",
+        "options": ["1/2", "2/3", "3/4", "4/5"],
+        "correctAnswer": "2/3"
+      },
+      {
+        "question": "What is the missing number in the pattern: 2, 4, __, 8, 10?",
+        "options": ["6", "12", "14", "16"],
+        "correctAnswer": "6"
+      },
+      {
+        "question": "Find the value of 'x' in the equation: 3x + 5 = 14.",
+        "options": ["3", "4", "5", "6"],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "What is the area of a rectangle with length 8 units and width 5 units?",
+        "options": ["13 square units", "25 square units", "40 square units", "45 square units"],
+        "correctAnswer": "40 square units"
+      }
+    ],
+    "Science": [
+      {
+        "question": "What is the process by which plants release water vapor into the air?",
+        "options": ["Evaporation", "Condensation", "Transpiration", "Precipitation"],
+        "correctAnswer": "Transpiration"
+      },
+      {
+        "question": "What do you call the study of living organisms and their interactions with the environment?",
+        "options": ["Botany", "Zoology", "Ecology", "Geology"],
+        "correctAnswer": "Ecology"
+      },
+      {
+        "question": "What is the unit used to measure electric current?",
+        "options": ["Volts", "Watts", "Amps", "Ohms"],
+        "correctAnswer": "Amps"
+      },
+      {
+        "question": "Which gas do plants take in from the air during photosynthesis?",
+        "options": ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+        "correctAnswer": "Carbon Dioxide"
+      },
+      {
+        "question": "What is the hardest known natural material on Earth?",
+        "options": ["Diamond", "Gold", "Silver", "Platinum"],
+        "correctAnswer": "Diamond"
+      },
+      {
+        "question": "Which planet is known as the 'Blue Planet'?",
+        "options": ["Earth", "Uranus", "Neptune", "Mercury"],
+        "correctAnswer": "Earth"
+      }
+    ],
+    "History": [
+      {
+        "question": "Who wrote the Declaration of Independence?",
+        "options": ["George Washington", "Abraham Lincoln", "Thomas Jefferson", "Benjamin Franklin"],
+        "correctAnswer": "Thomas Jefferson"
+      },
+      {
+        "question": "What did the Pilgrims celebrate with the Native Americans in 1621?",
+        "options": ["Halloween", "Thanksgiving", "Independence Day", "Christmas"],
+        "correctAnswer": "Thanksgiving"
+      },
+      {
+        "question": "Which famous African American woman is known for her efforts to end slavery through the Underground Railroad?",
+        "options": ["Harriet Tubman", "Rosa Parks", "Sojourner Truth", "Maya Angelou"],
+        "correctAnswer": "Harriet Tubman"
+      },
+      {
+        "question": "What important document begins with the words 'We the People'?",
+        "options": ["Declaration of Independence", "Bill of Rights", "Gettysburg Address", "Constitution of the United States"],
+        "correctAnswer": "Constitution of the United States"
+      },
+      {
+        "question": "Who was the 16th President of the United States and led the country during the Civil War?",
+        "options": ["George Washington", "Abraham Lincoln", "Thomas Jefferson", "John F. Kennedy"],
+        "correctAnswer": "Abraham Lincoln"
+      },
+      {
+        "question": "Which famous speech did Martin Luther King Jr. deliver in 1963?",
+        "options": ["I Have a Dream", "We Shall Overcome", "Give Me Liberty or Give Me Death", "Tear down this wall!"],
+        "correctAnswer": "I Have a Dream"
+      }
+    ],
+    "English": [
+      {
+        "question": "What is the plural form of the word 'child'?",
+        "options": ["childs", "childes", "childen", "children"],
+        "correctAnswer": "children"
+      },
+      {
+        "question": "Identify the correct sentence: 'The cat is sleeping on the mat.' or 'The cat are sleeping on the mat.'?",
+        "options": ["The cat is sleeping on the mat.", "The cat are sleeping on the mat."],
+        "correctAnswer": "The cat is sleeping on the mat."
+      },
+      {
+        "question": "Which word means the same as 'tiny'?",
+        "options": ["Huge", "Small", "Giant", "Enormous"],
+        "correctAnswer": "Small"
+      },
+      {
+        "question": "What is the past tense of the verb 'run'?",
+        "options": ["Ran", "Runned", "Running", "Runs"],
+        "correctAnswer": "Ran"
+      },
+      {
+        "question": "Choose the correct homophone: 'to', 'too', 'two', or 'tow'?",
+        "options": ["to", "too", "two", "tow"],
+        "correctAnswer": "too"
+      },
+      {
+        "question": "What is the synonym for the word 'happy'?",
+        "options": ["Sad", "Angry", "Joyful", "Scared"],
+        "correctAnswer": "Joyful"
+      }
+    ]
   }
 };
-
 //let currentSubject = null;
 //let currentQuestionIndex = 0;
 let currentGrade = null;
